177e7c129f2e3546d3555aa729f68a5c13adfaaf
Coderano0o <847130226@qq.com>
2025-01-15 10:27:40 +0800

    display hovering information 鼠标悬浮时显示详细信息

--- src/main/java/site/coderan/usefulbookshelf/gui/modscreens/UsefulBookshelfScreen.java
+++ src/main/java/site/coderan/usefulbookshelf/gui/modscreens/UsefulBookshelfScreen.java
@@ -6,17 +6,25 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import site.coderan.usefulbookshelf.ModMain;
 import site.coderan.usefulbookshelf.gui.modmenus.UsefulBookshelfMenu;
 
 public class UsefulBookshelfScreen extends AbstractContainerScreen<UsefulBookshelfMenu> {
     private static final ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID, "textures/gui/useful_bookshelf_inventory.png");
 
-    public UsefulBookshelfScreen(UsefulBookshelfMenu p_97741_, Inventory p_97742_, Component p_97743_) {
-        super(p_97741_, p_97742_, p_97743_);
+    public UsefulBookshelfScreen(UsefulBookshelfMenu usefulBookshelfMenu, Inventory inventory, Component component) {
+        super(usefulBookshelfMenu, inventory, component);
         this.imageWidth = 176;
         this.imageHeight = 150;
         this.inventoryLabelY = this.imageHeight - 94;
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        renderBg(guiGraphics, partialTicks, mouseX, mouseY);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+        renderTooltip(guiGraphics, mouseX, mouseY);
     }
 
     @Override
