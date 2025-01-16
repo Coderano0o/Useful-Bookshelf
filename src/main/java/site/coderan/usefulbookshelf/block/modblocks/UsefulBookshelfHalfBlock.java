87d43859a3f245b8e4c93cdfe3106a34583d2049
Coderano0o <847130226@qq.com>
2025-01-16 21:26:58 +0800

    update to v2.X

---
+++ src/main/java/site/coderan/usefulbookshelf/block/modblocks/UsefulBookshelfHalfBlock.java
@@ -0,0 +1,9 @@
+package site.coderan.usefulbookshelf.block.modblocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class UsefulBookshelfHalfBlock extends Block {
+    public UsefulBookshelfHalfBlock(Properties properties) {
+        super(properties);
+    }
+}
